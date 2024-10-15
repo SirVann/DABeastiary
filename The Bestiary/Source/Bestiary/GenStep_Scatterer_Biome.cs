ce6e2c96f353378febc8204070582be0911c2641
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2024-10-14 19:39:10 -0600

    Troll Burrow

---
+++ The Bestiary/Source/Bestiary/GenStep_Scatterer_Biome.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace Bestiary;
+public class GenStep_Scatterer_Biome : GenStep_ScatterThings
+{
+    public List<BiomeDef> allowedBiomes;
+    public override void Generate(Map map, GenStepParams parms)
+    {
+        if (allowedBiomes != null && allowedBiomes.Contains(map.Biome))
+        {
+            base.Generate(map, parms);
+        }
+    }
+
+}
