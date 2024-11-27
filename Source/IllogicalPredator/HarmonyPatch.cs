373a6a15f241db7f863987026304c58265d01de4
Robert <robhos2000@gmail.com>
2024-11-27 13:25:38 -0500

    Fixed illogical predator?

--- Source/IllogicalPredator/HarmonyPatch.cs
+++ Source/IllogicalPredator/HarmonyPatch.cs
@@ -24,22 +24,25 @@ namespace IllogicalPredator
             {
                 PredatorConditions conditions = predator.kindDef.GetModExtension<PredatorConditions>();
 
-                if (conditions.ignoreBodySize)
-                    // If body size should be ignored, check if combat power should be ignored or if the prey combat power is below the threshold
-                    __result = prey.BodySize > predator.RaceProps.maxPreyBodySize || prey.kindDef.combatPower > 2f * predator.kindDef.combatPower;
-                else
-                    // If body size shouldn't be ignored, double check the body size condition isn't what caused the false result, then check to make sure power is actually what caused the false result 
-                    __result |= prey.BodySize < predator.RaceProps.maxPreyBodySize && prey.kindDef.combatPower > 2f * predator.kindDef.combatPower;
+                // Checks to see if combat power was a reason that the vanilla check failed
+                __result |= prey.kindDef.combatPower > 2f * predator.kindDef.combatPower;
+
+                // If a basic cp check doesn't find anything, this checks if the weird combat power check was the reason for the failure
+                __result |= prey.kindDef.combatPower * prey.health.summaryHealth.SummaryHealthPercent * prey.BodySize >=
+                    predator.kindDef.combatPower * predator.health.summaryHealth.SummaryHealthPercent * predator.BodySize;
+
+                // If the pawn shouldn't be ignoring body size, then this checks to make sure body size wasn't the reason for the failure
+                if (!conditions.ignoreBodySize)
+                    __result &= prey.BodySize > predator.RaceProps.maxPreyBodySize;
 
                 // Check the conditions that normally appear after the body size checks in the vanilla method. The above situations are checking that the body size or combat power are what made the result false, but these need to be checked to ensure none of the future conditions would have also made it false
                 if (__result)
-                    if ((predator.Faction != null && prey.Faction != null && !predator.HostileTo(prey)) ||
-                        (predator.Faction != null && prey.HostFaction != null && !predator.HostileTo(prey)) ||
-                        (predator.Faction == Faction.OfPlayer && prey.Faction == Faction.OfPlayer) ||
-                        (predator.RaceProps.herdAnimal && predator.def == prey.def) ||
-                        (prey.IsHiddenFromPlayer() || prey.IsPsychologicallyInvisible()) ||
-                        (ModsConfig.AnomalyActive && prey.IsMutant && !prey.mutant.Def.canBleed))
-                        __result = false;
+                    __result &= ((predator.Faction == null || prey.Faction == null || predator.HostileTo(prey)) &&
+                        (predator.Faction == null || prey.HostFaction == null || predator.HostileTo(prey)) &&
+                        (predator.Faction != Faction.OfPlayer || prey.Faction != Faction.OfPlayer) &&
+                        (!predator.RaceProps.herdAnimal || predator.def != prey.def) &&
+                        (!prey.IsHiddenFromPlayer() && !prey.IsPsychologicallyInvisible()) &&
+                        (!ModsConfig.AnomalyActive || !prey.IsMutant || prey.mutant.Def.canBleed));
             }
         }
     }
